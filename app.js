186adf686059a2da7b3d924bf2b3102364719f33
Jessica Claire Edwards <jessica@eml.cc>
2015-07-29 11:57:15 +1000

    Implemented fizz buzz

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function calculateNumericSequences(event) {
   numericSequences.push(naturalNumbers(number));
   numericSequences.push(oddNumbers(number));
   numericSequences.push(evenNumbers(number));
+  numericSequences.push(fizzBuzz(number));
   console.log(numericSequences.join("\n"));
   // $('#result').text(numericSequences.join("\n"));
 }
@@ -32,6 +33,25 @@ function evenNumbers(number) {
   var result = [];
   for (var i = 2; i <= number; i += 2) {
     result.push(i);
+  }
+  return result.join(', ');
+}
+
+function fizzBuzz(number) {
+  var result = [];
+  for (var i = 1; i <= number; i++) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+      result.push('Z');
+    }
+    else if (i % 3 == 0) {
+      result.push('C');
+    }
+    else if (i % 5 == 0) {
+      result.push('E');
+    }
+    else {
+      result.push(i);
+    }
   }
   return result.join(', ');
 }
