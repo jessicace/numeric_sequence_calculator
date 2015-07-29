2733efcc222e4844295705f5069f486cb699da73
Jessica Claire Edwards <jessica@eml.cc>
2015-07-29 11:51:39 +1000

    Calculate even number sequence

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ function calculateNumericSequences(event) {
   var numericSequences = [];
   numericSequences.push(naturalNumbers(number));
   numericSequences.push(oddNumbers(number));
+  numericSequences.push(evenNumbers(number));
   console.log(numericSequences.join("\n"));
-  $('#result').text(numericSequences.join("\n"));
+  // $('#result').text(numericSequences.join("\n"));
 }
 
 function naturalNumbers(number) {
@@ -22,6 +23,14 @@ function naturalNumbers(number) {
 function oddNumbers(number) {
   var result = [];
   for (var i = 1; i <= number; i += 2) {
+    result.push(i);
+  }
+  return result.join(', ');
+}
+
+function evenNumbers(number) {
+  var result = [];
+  for (var i = 2; i <= number; i += 2) {
     result.push(i);
   }
   return result.join(', ');
