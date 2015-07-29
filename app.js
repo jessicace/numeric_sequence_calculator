cf7259b395199b1d59257d4f40a8715b76b25225
Jessica Claire Edwards <jessica@eml.cc>
2015-07-29 11:47:24 +1000

    Refactored

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 window.onload = function () {
-  $("#calculate").click(naturalNumbers);
+  $('#calculate').click(calculateNumericSequences);
+}
 
-  function naturalNumbers(event) {
-    var result = [];
-    var number = $('#number').val();
-    console.log(number);
-    for (var i = 1; i <= number; i++) {
-      result.push(i);
-    }
-    console.log(result);
-    $('#result').text(result.join(', '));
+function calculateNumericSequences(event) {
+  var number = $('#number').val();
+  var numericSequences = [];
+  numericSequences.push(naturalNumbers(number));
+  numericSequences.push(oddNumbers(number));
+  console.log(numericSequences.join("\n"));
+  $('#result').text(numericSequences.join("\n"));
+}
+
+function naturalNumbers(number) {
+  var result = [];
+  for (var i = 1; i <= number; i++) {
+    result.push(i);
   }
+  return result.join(', ');
+}
+
+function oddNumbers(number) {
+  var result = [];
+  for (var i = 1; i <= number; i += 2) {
+    result.push(i);
+  }
+  return result.join(', ');
 }
